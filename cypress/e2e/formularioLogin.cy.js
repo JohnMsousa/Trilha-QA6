97002a62fde96735961ccbf021ca1a942b9773c3
JohnMsousa <johnmsousasantos@gmail.com>
2025-01-26 22:32:03 -0300

    test: adiciona requisições GET e interceptação de POST com Cypress

---
+++ cypress/e2e/formularioLogin.cy.js
@@ -0,0 +1,26 @@
+describe('Formulario de Login', () => {
+  it.only('Deve acessar a página home', () => {
+    cy.login('neilton@alura.com', '123456');
+    cy.visit('/home');
+    cy.getByData('titulo-boas-vindas').should('contain', 'Bem vindo de volta!');
+  });
+
+  it('Não deve permitir um email inválido', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('email-input').type('neilton@alura.com');
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O email digitado é inválido');
+  });
+
+  it('Não deve permitir um campo em branco', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo email é obrigatório');
+  });
+});
